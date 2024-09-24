94cc2c37c45992335a94120b3f57be285e845a66
Wahyu Ramadhan <wahyuramadhan@fanintek.com>
2024-09-24 16:54:41 +0700

    chore : config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          light: '#fff',
+          dark: '#21282e',
+        },
+        secondary: {
+          light: '#21282e',
+          dark: '#00E0FF',
+        },
+        tertiary: {
+          light: '#21282e',
+          dark: '#C4C4C4',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
